5a2afcee797448bdb6d5f73fe1a5892843a6f70a
Maryam6713 <maryammalik6713@gmail.com>
2024-05-06 17:31:28 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+//-------------------Restaurent Bill---------------------//
+
+
+//-----------About-----------//
+var start = "------------------------------------"
+console.log(start)
+var Restaurent = "Restaurent Bill"
+console.log(Restaurent)
+var end = "-------------------------------------------"
+console.log(end)
+
+//-----------Define Charge----------//
+var biryani = 400;
+var chickenKorma = 350;
+var bbq = 300;
+var pizza = 1200;
+var zinger = 450;
+var iceCream = 500;
+var falooda = 800;
+
+//----------- Calculate Total Charges---------//
+var total = biryani + chickenKorma + bbq + pizza + zinger + iceCream + falooda;
+
+
+//---Define Charges of verity---//
+
+console.log("Charges")
+console.log("------------------")
+console.log("Biryani:  Rs." + biryani)
+console.log("Chicken Korma: Rs." + chickenKorma)
+console.log("BBQ:  Rs." + bbq)
+console.log("Large Pizza:  Rs." + pizza)
+console.log("Zinder Burger:  Rs." + zinger)
+console.log("Ice-Cream:  Rs." + iceCream)
+console.log("Falooda:  Rs." + falooda)
+
+//-------Define Total Charges------//
+console.log("-------------------")
+console.log("Total Charges: Rs." + total)
+console.log("--------------------")
+console.log("---------PAID--------")
